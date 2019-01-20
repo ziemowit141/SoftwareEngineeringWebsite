8d44f7cf296681e6ecb5be7587cb5fd01a563489
ziemowit141 <ziemowit141@gmail.com>
2019-01-20 22:47:31 +0100

    Added availableSupervisors and supervisorDetail view, u can open list of supervisiors, then check details of interesting one, + eventually go back

--- dissertationProcess/dissertation/urls.py
+++ dissertationProcess/dissertation/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('index/', views.index , name='index'),
     path('index/<username>/', views.UserIndexView.as_view(), name='userindex'),
+    path('supervisorsList/', views.SupervisorsList.as_view(), name='supervisorsList'),
+    path('supervisorsList/<str:username>/', views.SupervisorDetail.as_view(), name='supervisorsListDetail'),
     path('change-password/', auth_views.PasswordChangeView.as_view(template_name='dissertation/password_change_form.html'),name='password_change'),
     path('password-changed/', auth_views.PasswordChangeDoneView.as_view(template_name='dissertation/password_change_done.html'),name='password_change_done'),
 ]
--- dissertationProcess/dissertation/views.py
+++ dissertationProcess/dissertation/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
-
+from dissertation.models import Profile
 
 class StartView(TemplateView):
     template_name = 'dissertation/start.html'
@@ -38,3 +38,37 @@ class LogoutView(RedirectView):
         if self.request.user.is_authenticated:
             logout(self.request)
         return super(LogoutView, self).get_redirect_url(*args, **kwargs)
+
+
+class SupervisorsList(TemplateView):
+    template_name = 'dissertation/availableSupervisors.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(SupervisorsList, self).get_context_data(**kwargs)
+    #     context['supervisorsList'] = Profile.objects.filter(is_reviewer=True)
+        
+    #     return context
+
+    def get_context_data(self, **kwargs):
+        allProfiles = Profile.objects.all()
+        allSupervisorProfiles = []
+
+        for profile in allProfiles:
+            if profile.is_reviewer:
+                allSupervisorProfiles.append(profile)
+                print(profile.user.first_name)
+
+        context = {"supervisorsList" : allSupervisorProfiles}
+
+        return context
+
+
+class SupervisorDetail(TemplateView):
+    template_name = 'dissertation/supervisorDetail.html'
+
+    def get_context_data(self, username):
+        profile = Profile.objects.get(user__username=username)
+        print(profile.user.username)
+        context = {"supervisor" : profile}
+
+        return context    
